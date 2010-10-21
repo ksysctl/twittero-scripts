da6707c347ea42d792405770d7c3585b7a477f75
Moises Brenes <moises.brenes@gmail.com>
2010-10-21 17:57:56 -0600

    Added banshee plugin

---
+++ banshee.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------
+# Twittero - Powerful Twitter client
+# Banshee Script for Twittero
+# -----------------------------------------------------------
+# $author: Moises Brenes <moises.brenes@gmail.com>
+# $license: BSD License - $depends: songbird - dbus - mpris
+# -----------------------------------------------------------
+
+import dbus
+
+try:
+	bus = dbus.SessionBus()
+	o = bus.get_object('org.bansheeproject.Banshee', '/org/bansheeproject/Banshee/PlayerEngine')
+except:
+	print '♫♪ #Listening'
+	exit(1)
+
+data = o.GetCurrentTrack()
+print '#Listening ♫♪ {0} by #{1} from {2} using #Banshee'.format(data['name'], data['artist'], data['album'])
